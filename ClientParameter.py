8142341c1d130eda78b8001349853f4863b27b2b
John Carlee <jcarlee@gmail.com>
2018-05-14 18:16:07 -0500

    lists to string work

--- ClientParameter.py
+++ ClientParameter.py
@@ -30,23 +30,33 @@ def rest_query():
 
     # Ask for uGRIDD Field Names until user types STOP
     i = ''
+    rest_ugridd_str = ''
     while i != 'STOP':
         i = input('Enter uGRIDD Bubble Field Names, type STOP when done: ')
         rest_ugridd.append(i)
+        rest_ugridd_str = '","'.join(rest_ugridd)
+
     # Ask for field name and type based on list length
     for x, y in range(0, len(rest_ugridd)):
         a = input('Enter REST Field Name for' + rest_ugridd[x] + ': ')
         b = input('Enter REST Field definition (atr, link) for ' + rest_ugridd[y] + ': ')
         rest_field.append(a)
         rest_type.append(b)
-
+    rest_field_str = ''
+    prev = None
+    for a, b in rest_field, rest_type:
+        if prev is not None:
+            rest_field_str.append('"' + b + ':' + a + '",')
+        else:
+            rest_field_str.append('"' + b + ':' + a + '"')
+        prev = a
     # Ask for style string
     style = input('Style (#000000",0.8,2,"#005CE6",0.35): ')
 
     # Return final string
     return '["{0}?inSR=4326&outSR=4326&spatialRel=esriSpatialRelIntersects&f=json&returnGeometry=true&outFields=*&' \
            'distance=#distance#&units=esriSRUnit_Meter&supportsQueryWithDistance=true&geometryType=#geometryType#&' \
-           'geometry=#geometry#","Polygon","atr:{1}]'.format(url, title)
+           'geometry=#geometry#","Polygon","atr:{1}",["{2}"],["]'.format(url, title, rest_ugridd_str)
 
 
 def tile_export():
